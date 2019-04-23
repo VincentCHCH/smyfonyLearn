06168b315a96fdd852b2ac5e5941315778c6fa6b
Chen, Hao (Vincent) [MEDCN] <hchen180@its.jnj.com>
2019-04-23 15:00:10 +0800

    change head

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -58,6 +58,15 @@ class UserController extends AbstractController
             'activityFeed' => $activityFeed,
         ];
 
-        return $this->json($response, $statusCode);
+        return $this->json($response, $statusCode,
+            [
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Credentials' => 'true',
+                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'DNT, X-User-Token, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type',
+                'Access-Control-Max-Age' => 1728000,
+                'Content-Type' => 'text/plain charset=UTF-8',
+                'Content-Length' => 0
+            ]);
     }
 }
